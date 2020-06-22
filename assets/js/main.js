ce2a19e7b618e5f43e8971b71d7556dab685add7
Gabriel Carvalho <gabrielc_1997@hotmail.com>
2020-06-22 19:21:18 -0300

    home finalizada

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+function myFunction(x) {
+    $('.js-tilt').tilt({
+        scale: 1
+
+    })
+}
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+$(document).ready(function(){
+    $('.slick-carrosel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+    });
+  });
